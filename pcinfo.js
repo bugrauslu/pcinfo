911506df42701cd1afe2daddcefda4d290ee09f4
Buğra Uslu <59747293+bugrauslu@users.noreply.github.com>
2021-11-25 17:17:20 +0300

    Add files via upload

---
+++ pcinfo.js
@@ -0,0 +1,36 @@
+const os =require("os");
+const fs=require("fs");
+
+
+
+
+let pcinfo=function() {
+
+    let freeram =bytetogb(os.freemem());
+    //kullanılmayan ram 
+    let totalram=bytetogb(os.totalmem());
+    //toplam ram 
+    let usedram=(totalram-freeram).toFixed(2);
+    //kullanılan ram 
+    let cpusayisi=os.cpus().length;
+    //cpusayisi
+
+    fs.writeFile('pc_info.txt',pcinfogoster(), (err)=>{
+        if(err) console.log("yazdırma sırasında hata oluştu");
+        });
+ 
+        function bytetogb(toplambyte) {
+            return (toplambyte/1024/1024/1024).toFixed(2);
+        }
+        //byte to gb convert
+        
+        function pcinfogoster() {
+            return `total ram :${totalram} , kullanılan ram: ${usedram} ,free ram ${freeram}, cpu sayısı : ${cpusayisi}`;
+        }
+        
+        
+}
+
+
+
+exports.sendinfo=pcinfo;
